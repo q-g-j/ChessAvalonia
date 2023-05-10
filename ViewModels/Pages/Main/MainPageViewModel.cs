48278318857964e941c06244fdf01dd0ad6e3365
QGJ <qgj.devel@gmail.com>
2023-05-10 18:26:19 +0200

    clean up

--- ViewModels/Pages/Main/MainPageViewModel.cs
+++ ViewModels/Pages/Main/MainPageViewModel.cs
@@ -733,24 +733,6 @@ public partial class MainPageViewModel
         CanvasImages.Clear();
 
         int t = 0;
-        //{
-        //    for (int col = 1; col < 9; col++, t++)
-        //    {
-        //        int l = 0;
-        //        for (int row = 8; row > 0; row--, l++)
-        //        {
-        //            CanvasImage cell = new()
-        //            {
-        //                CellName = Coords.IntsToCoordsString(row, col),
-        //                CanvasLeft = l * 60,
-        //                CanvasTop = t * 60,
-        //                Image = ChessPieceImages.Empty,
-        //            };
-
-        //            CanvasImages.Add(cell);
-        //        }
-        //    }
-        //}
         for (int col = 8; col > 0; col--, t++)
         {
             int l = 0;
